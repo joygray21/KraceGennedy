0514a839a523d86e247c81c64828f518d76b4899
Joanna-Joy Gray <jjgray21@hotmail.com>
2021-05-18 18:06:28 -0500

    Rough print out of forecast data

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for
-
+import requests #python library
 
 ###
 # Routes
@@ -11,6 +11,17 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/api/forecast', methods=['GET'])
+def get_forecast():
+    cities = ['Montego Bay', 'Kingston'] #for scalability, retrieve unique cities from db
+    forecasts = {} #dictionary with each city and a list of its forecasts for the next 5 days
+
+    for city in cities:
+        req = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={APIKey}'.format(city=city, APIKey='#')) 
+        c = req.content
+        #forecasts[city] = 
+    
+    return render_template('home.html', data = c)
 
 
 ### run statement
